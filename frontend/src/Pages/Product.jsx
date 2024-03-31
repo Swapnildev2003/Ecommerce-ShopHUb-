3d17368104e7e82fa93c77db1dc4589bcb7355c1
Prasanshaa <160015618+Prasanshaa@users.noreply.github.com>
2024-04-01 00:21:22 +0530

    frontend cart updated

--- frontend/src/Pages/Product.jsx
+++ frontend/src/Pages/Product.jsx
@@ -3,6 +3,8 @@ import { useParams } from "react-router-dom";
 import { ShopContext } from "../Context/ShopContext";
 import Breadcrums from "../components/Breadcrums/Breadcrum";
 import ProductDisplay from "../components/ProductDisplay/ProductDisplay";
+import DescriptionBox from "../components/DescriptionBox/DescriptionBox";
+import RelatedProducts from "../components/RelatedProducts/RelatedProduct";
 
 const Product = () => {
   const { all_product } = useContext(ShopContext);
@@ -12,6 +14,8 @@ const Product = () => {
     <div>
       <Breadcrums product={product} />
       <ProductDisplay product={product} />
+      <DescriptionBox/>
+      <RelatedProducts/>
     </div>
   );
 };
