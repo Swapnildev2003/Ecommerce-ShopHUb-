5255b0cbe95fba438f4ee84bf8d4e3a25b1788a2
Navya-1212 <navyasrivastava1212@gmail.com>
2024-05-14 14:51:10 +0530

    cart changes done

--- frontend/src/components/CartItems/CartItems.jsx
+++ frontend/src/components/CartItems/CartItems.jsx
@@ -63,9 +63,9 @@ const CartItems = () => {
               <div className="cartitems-format cartitems-format-main">
                 <img src={e.image} alt="" className='carticon-product-icon' />
                 <p>{e.name}</p>
-                <p>${e.new_price}</p>
+                <p>₹{e.new_price}</p>
                 <button className='cartitems-quantity'>{cartItems[e.id]}</button>
-                <p>${e.new_price * cartItems[e.id]}</p>
+                <p>₹{e.new_price * cartItems[e.id]}</p>
                 <img className="cartitems-remove-icon" src={remove_icon} onClick={() => { removeFromCart(e.id) }} alt="" />
               </div>
               <hr />
